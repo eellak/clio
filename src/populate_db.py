7a9cc41bccb974191462689f79039ee12b3c508e
Gopalakrishnan.V <gopuvenkat@yahoo.com>
2018-05-26 20:31:12 +0530

    populate component_conn table

--- src/populate_db.py
+++ src/populate_db.py
@@ -4,8 +4,10 @@ from models import *
 from app import db
 from datetime import datetime
 
+
 def populate_license(directory):
-    path = os.path.join(os.getcwd(), os.path.join(directory, 'input-license.csv'))
+    path = os.path.join(os.getcwd(), os.path.join(
+        directory, 'input-license.csv'))
     with open(path) as input_file:
         read_csv = csv.reader(input_file, delimiter='|')
         for row in read_csv:
@@ -22,12 +24,14 @@ def populate_license(directory):
             license_category = row[4]
             license_text = row[5]
             l = License(full_name, identifier, fsf_free_libre,
-                        osi_approved, license_category, license_text)
+                    osi_approved, license_category, license_text)
             db.session.add(l)
             db.session.commit()
 
+
 def populate_component(directory):
-    path = os.path.join(os.getcwd(), os.path.join(directory, 'input-component-info.csv'))
+    path = os.path.join(os.getcwd(), os.path.join(
+        directory, 'input-component-info.csv'))
     with open(path) as input_file:
         read_csv = csv.reader(input_file, delimiter='|')
         for row in read_csv:
@@ -41,9 +45,24 @@ def populate_component(directory):
             source_url = row[5]
             license_expression = row[6]
             ext_link = row[7]
-            c = Component(name, version, created_by, pub_date, origin, source_url, license_expression, ext_link)
+            c = Component(name, version, created_by, pub_date,
+                    origin, source_url, license_expression, ext_link)
             db.session.add(c)
             db.session.commit()
+
+def populate_component_conn(directory):
+    path = os.path.join(os.getcwd(), os.path.join(
+        directory, 'input-component-relationship.csv'))
+    with open(path) as input_file:
+        read_csv = csv.reader(input_file, delimiter='|')
+        for row in read_csv:
+            input_c1_name = row[0]
+            c1 = Component.query.filter_by(name=input_c1_name).first()
+            input_c2_name = row[2]
+            c2 = Component.query.filter_by(name=input_c2_name).first()
+            c1.components.append(c2)
+            db.session.commit()
+
 
 if __name__ == '__main__':
     # Recreate DB
@@ -53,3 +72,4 @@ if __name__ == '__main__':
     directory = 'dataset'
     populate_license(directory)
     populate_component(directory)
+    populate_component_conn(directory)
